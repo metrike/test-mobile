96c602e5c60dd58286fe8d6bfee7b4d9cc66e2d3
metrike <148220966+metrike@users.noreply.github.com>
2025-02-04 15:04:41 +0100

    Update transfert_script.ps1

--- transfert_script.ps1
+++ transfert_script.ps1
@@ -10,8 +10,16 @@ if (-Not (Test-Path -Path $source)) {
 
 # Vérifie si le fichier de destination existe
 if (Test-Path -Path $destination) {
-    Write-Output "Le fichier existe déjà à la destination. Ajout du contenu du fichier source."
-    # Ajoute le contenu du fichier source à la fin du fichier de destination
+    Write-Output "Le fichier existe déjà à la destination. Vérification pour ajout de contenu."
+    
+    # Vérifie si le fichier de destination se termine déjà par une ligne vide
+    $lastLine = Get-Content -Path $destination | Select-Object -Last 1
+    if ($lastLine -ne "") {
+        Write-Output "Ajout d'un retour à la ligne au fichier de destination."
+        Add-Content -Path $destination -Value "`r`n"
+    }
+    
+    # Ajoute le contenu du fichier source à la suite du fichier de destination
     Get-Content -Path $source | Add-Content -Path $destination
     Write-Output "Contenu ajouté avec succès au fichier de destination."
 } else {
